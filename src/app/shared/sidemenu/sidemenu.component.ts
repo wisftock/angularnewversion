b63ba5eb0e70e8a0ee4da014ad70749e1dd1c154
wisftock <fullstack.sw@gmail.com>
2024-03-01 15:33:15 -0500

    feat: use new syntax

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css',
 })
