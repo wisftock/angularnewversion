606d3aa92a13c6a010aad6bcad7bdd1db8500073
wisftock <fullstack.sw@gmail.com>
2024-03-06 12:57:23 -0500

    feat: add dashboard

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow.component.html',
-  styleUrl: './control-flow.component.css'
+  styleUrl: './control-flow.component.css',
 })
 export class ControlFlowComponent {
+  public showContent = signal(false);
 
+  public toggleContent() {
+    this.showContent.update((item) => !item);
+  }
 }
