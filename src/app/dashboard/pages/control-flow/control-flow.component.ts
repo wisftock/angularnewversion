7283f450b9e77517cecfcb85ad5c596dd3923e8b
wisftock <fullstack.sw@gmail.com>
2024-03-07 10:16:27 -0500

    feat: add conditionals

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+type Grade = 'A' | 'B' | 'C';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css',
 })
 export class ControlFlowComponent {
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public tecnologia = signal(['react', 'angular', 'vue', 'qwit']);
+  public tecnologia2 = signal([]);
 
   public toggleContent() {
     this.showContent.update((item) => !item);
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
   templateUrl: './title.component.html',
-  styles: ``
+  styles: ``,
 })
 export class TitleComponent {
-
+  @Input({ required: true }) title!: string;
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
 }
