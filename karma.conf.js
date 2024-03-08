2f83addce625b369d2c8af96aa964e335439eb7c
wisftock <fullstack.sw@gmail.com>
2024-03-08 04:32:18 -0500

    feat: testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,100 +1,52 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ["jasmine-jquery", "jasmine", "fixture"],
-    plugins: ["karma-"],
-    preprocessors: {
-      "test/jasmine/spec/Rio/**/.js": ["babel"],
-      "src/webapp/Rio/js/.js": ["babel"],
-      "src/webapp/Rio/js/!(vendor|component)/**/.js": ["babel"],
-    },
-    babelPreprocessor: {
-      options: {
-        sourceMap: "inline",
-        presets: ["@babel/preset-env"],
-        plugins: config.server
-          ? [
-              "@babel/plugin-proposal-optional-chaining",
-              "@babel/plugin-proposal-nullish-coalescing-operator",
-            ]
-          : [
-              [
-                "istanbul",
-                {
-                  exclude: ["test/jasmine/spec/**/*.js"],
-                },
-              ],
-              "@babel/plugin-proposal-optional-chaining",
-              "@babel/plugin-proposal-nullish-coalescing-operator",
-            ],
-        overrides: [
-          {
-            sourceType: "script",
-          },
-        ],
-      },
-    },
-    coverageIstanbulReporter: {
-      reports: ["html", "cobertura", "text-summary"],
-      dir: "target/coverage/rio/",
-      thresholds: {
-        global: {
-          lines: 99.17,
-          statements: 99.14,
-          branches: 94.17,
-          functions: 98,
-        },
-      },
-    },
-    specReporter: {
-      suppressErrorSummary: false,
-      suppressFailed: false,
-      suppressPassed: false,
-      suppressSkipped: process.env.NODE_ENV === "debug",
-      showSpecTiming: false,
-      failFast: false,
-    },
-    client: {
-      jasmine: {
-        random: false,
-        hideDisabled: true,
-      },
-    },
-    reporters: ["progress", "coverage-istanbul", "spec", "summary", "kjhtml"],
-    port: 9876, // karma web server port
-    colors: true,
-    logLevel: config.LOG_DEBUG,
-    browsers: ["PhantomJS, ChromeHeadlessCI", "ChromeHeadless"],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: "ChromeHeadless",
-        flags: ["--no-sandbox"],
-      },
+    // Rutas base que se usarán para resolver todos los archivos y patrones
+    basePath: "",
 
-      OmniChrome: {
-        base: "Chrome",
-        flags: [
-          "--start-maximized",
-          "--allow-file-access-from-files",
-          "--disable-translate",
-          "--disable-extensions",
-          "--remote-debugging-port=9223",
-          "--auto-open-devtools-for-tabs",
-          "http://localhost:9876/debug.html",
-        ],
-      },
+    // Marco de trabajo de pruebas que se utilizará
+    frameworks: ["jasmine"],
+
+    // Archivos a incluir, lista de archivos/patrones a cargar en el navegador
+    files: [
+      // Todos los archivos de pruebas
+      "tests/**/*.spec.js",
+    ],
+
+    // Lista de archivos a excluir
+    exclude: [],
+
+    // Lista de complementos a cargar
+    plugins: ["karma-jasmine", "karma-chrome-launcher", "karma-junit-reporter"],
+
+    // Navegadores a utilizar para ejecutar las pruebas
+    browsers: ["ChromeHeadless"],
+
+    // Formatos de informes
+    reporters: ["progress", "junit"],
+
+    // Configuración de informes JUnit para integración con GitLab CI
+    junitReporter: {
+      outputDir: "test_results", // Directorio de salida para los informes JUnit
+      outputFile: "test-results.xml", // Nombre del archivo de informe JUnit
+      useBrowserName: false, // Deshabilita el nombre del navegador en los informes JUnit
     },
-    autoWatch: true,
-    singleRun: false,
+
+    // Puerto del servidor web
+    port: 9876,
+
+    // Nivel de registro
+    logLevel: config.LOG_INFO,
+
+    // Habilita/deshabilita colores en la salida de la consola
+    colors: false,
+
+    // Tiempo de espera en milisegundos para reiniciar el servidor de pruebas
+    autoWatch: false,
+
+    // Configuración de pruebas en tiempo real (TDD)
+    singleRun: true,
+
+    // Tiempo de espera en milisegundos entre ejecución de pruebas
     concurrency: Infinity,
   });
 };
-
-// browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
-//     singleRun: false,
-//     restartOnFileChange: true,
-//     customLaunchers: {
-//       ChromeHeadlessCI: {
-//         base: "ChromeHeadless",
-//         flags: ["--no-sandbox"],
-//       },
-//     },
