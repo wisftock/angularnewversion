09ffd53583fc2bf988cec11f2a3dae7ea760dc76
wisftock <fullstack.sw@gmail.com>
2024-03-08 04:04:55 -0500

    feat: testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,12 @@
 module.exports = function (config) {
   config.set({
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
     browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
     customLaunchers: {
       ChromeHeadlessCI: {
