cbee396e9e8d22cbeb9c8818181ad8bd9e39c666
wisftock <fullstack.sw@gmail.com>
2024-03-08 04:08:58 -0500

    feat: testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,10 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
@@ -7,7 +12,25 @@ module.exports = function (config) {
       require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
-    browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/profanis-yt"),
+      subdir: ".",
+      reporters: [{ type: "html" }, { type: "text-summary" }],
+    },
+    reporters: ["progress", "kjhtml"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["Chrome", "ChromeHeadlessCI"],
+    singleRun: false,
+    restartOnFileChange: true,
     customLaunchers: {
       ChromeHeadlessCI: {
         base: "ChromeHeadless",
